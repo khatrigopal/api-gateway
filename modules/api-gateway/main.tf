5f42351e58e5abd9c145fbe0ce6230df442579c1
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-14 07:38:25 +0000

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -8,7 +8,8 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "my-resource"
+  #path_part   = "my-resource"
+  path_part   = var.resource
 }
 
 resource "aws_api_gateway_method" "method" {
