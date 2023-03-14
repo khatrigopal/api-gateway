097252374ebc6ffffd4100bfc45fbbe697edec6b
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-14 08:20:53 +0000

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -16,5 +16,6 @@ resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
+  http_method   = var.http_method
   authorization = "NONE"
 }
