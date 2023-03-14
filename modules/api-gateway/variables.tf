1ca07d623a5dd0c1f6f688d321679fe7cb753dc7
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-14 11:04:37 +0530

    Create variables.tf

---
+++ modules/api-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "api_name" {
+  type        = string
+  description = "The name of the API Gateway"
+}
+
+variable "protocol" {
+  type        = string
+  description = "The protocol used by the API Gateway (HTTP or HTTPS)"
+}
