8d6c968fa4ee31309b205328d77931584b69f1f4
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-15 06:29:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "my_api_gateway" {
   source   = "./modules/api-gateway"
   #version  = "1.0.0"
-  api_name = var.api_name
+  api_name = my-api-gateway
+  #api_name = var.api_name
   protocol = "HTTP"
   resource   = "my-resource"
   http_method = "POST"
